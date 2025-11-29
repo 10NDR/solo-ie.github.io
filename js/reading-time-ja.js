43090a998e914a70a6eb5b38a2d48fcd6b205679
10NDR <10NDR@users.noreply.github.com>
2025-11-29 00:39:52 +0000

    deploy: 10NDR/solo-ie-source@a373d10b73b4903dba6f16ed7c5227235ebcd58f

--- js/reading-time-ja.js
+++ js/reading-time-ja.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // テキスト抽出・空白除去
   var text = content.innerText.replace(/\s+/g, '');
   var length = text.length;
-  var speed = 500; // 1分あたりの文字数
+  var speed = 400; // 1分あたりの文字数
   var minutes = Math.max(1, Math.round(length / speed));
   var msg = length < speed ? '読了目安：1分未満' : '読了目安：' + minutes + '分';
   target.textContent = msg;
